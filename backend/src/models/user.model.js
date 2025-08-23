d228f8aae6a3e8392617f7b335884b3701f56bd8
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-08-23 21:19:04 +0530

    add User model with auth (hashing, JWT, refresh tokens)

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    userName : {
+        type : String,
+        required : [true, "Please add a username"],
+        unique : true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : [true, "Please add an email"],
+        unique : true,
+        match : [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please add a valid email",
+        ],
+    },
+    password : {
+        type : String,
+        required : [true, "Please add a password"],
+        minlength : [6, "Please add atleast 6 digit password"],
+        select : false // hide password in queries
+    },
+    profilePic : {
+        type : String,
+        default : "https://i.ibb.co/MBtjqXQ/default-profile.png",
+    },
+    isAdmin : {
+        type : Boolean,
+        default : false
+    },
+    myList : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Movie"
+        }
+    ]
+},{timestamps : true});
+
+
+// Encrypt password before saving
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password"))
+        return next();
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Method to match password during login
+userSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+// Method to generate JWT
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            id : this._id,
+            email : this.email,
+            isAdmin : this.isAdmin
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : "1d"
+        }
+    );
+};
+
+// Method to generate Refress JWT
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : "7d"
+        }
+    );
+};
+
+export const User = mongoose.model("User", userSchema);
