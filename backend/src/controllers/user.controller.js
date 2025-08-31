9ec9dee0893b44cd5c6964ee2497692540b51e26
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-09-01 01:46:05 +0530

    adding login Controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -31,12 +31,50 @@ export const registerUser = asyncHandler(async(req, res) => {
         password,
         avatar : avatar.url,
     });
-    const createdUser = await User.findOne(user?._id).select(" -password -refreshToken");
+    const createdUser = await User.findById(user?._id).select(" -password -refreshToken");
     if(!createdUser){
         throw new ApiError(500, "Something went wrong to createdUser");
     }
     res.status(201).json({
         message : "User registered successfully",
-        user
+        user : createdUser
+    });
+});
+
+export const loginUser = asyncHandler(async(req, res) => {
+    const {email, password } = req.body;
+    if([email, password].some((field) => field?.trim() === "")){
+        throw new ApiError(400, "Email and password required");
+    }
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        throw new ApiError(401, "Invalid credentials");
+    }
+    const isMatch = await user.matchPassword(password);
+    if(!isMatch){
+        throw new ApiError(401, "Invalid credentials");
+    }
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({
+        validateBeforeSave : false
+    });
+    res.cookie("userToken", accessToken, {
+        httpOnly : true,
+        secure : process.env.NODE_ENV === "production",
+        maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly : true,
+        secure : process.env.NODE_ENV === "production",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    const userData = await User.findById(user?._id).select("-password -refreshToken");
+    res.status(200).json({
+        message : "Login successful",
+        user : userData,
+        accessToken,
+        refreshToken,
     });
 });
