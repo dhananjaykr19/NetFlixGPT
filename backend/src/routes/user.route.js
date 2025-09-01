fc3d37a10f71603f06cef0a99a044ffc6f80c8c8
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-09-01 22:08:15 +0530

    adding logout router

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { protectedUser } from "../middlewares/authUser.middleware.js";
 
 const router = Router();
 
@@ -14,4 +15,5 @@ router.route("/register").post(
     registerUser
 );
 router.route("/login").post(loginUser);
+router.route("/logout").post(protectedUser, logoutUser);
 export default router;
