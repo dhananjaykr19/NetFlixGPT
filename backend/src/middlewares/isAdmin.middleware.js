767c03c2f53066bfd7aae2c6b8c4c90f040e6293
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-09-04 20:32:24 +0530

    syntax change

--- backend/src/middlewares/isAdmin.middleware.js
+++ backend/src/middlewares/isAdmin.middleware.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 
-export const requireRole = (role) => {
+export const isAdmin = (role) => {
     return (req, res, next) => {
         if(req.user && req.user.role === role){
             next();
