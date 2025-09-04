e1dd3306f8f731734176e6b4900c675c76cea206
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-09-04 20:32:52 +0530

    adding the upload fields

--- backend/src/routes/movie.route.js
+++ backend/src/routes/movie.route.js
@@ -1,13 +1,32 @@
 import { Router } from "express";
 import { protectAdmin } from "../middlewares/authAdmin.middleware.js";
-import { requireRole } from "../middlewares/isAdmin.middleware.js";
+import { isAdmin } from "../middlewares/isAdmin.middleware.js";
 import { addMovie, deleteMovie, getAllMovies, getMovieById, updateMovie } from "../controllers/movie.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/").get(getAllMovies);
 router.route("/:movieId").get(getMovieById);
-router.route("/add").post(protectAdmin, requireRole("super_admin"), addMovie);
-router.route("/:movieId").put(protectAdmin, requireRole("super_admin"), updateMovie);
-router.route("/:movieId").delete(protectAdmin, requireRole("super_admin"), deleteMovie);
+router.route("/add").post(protectAdmin, isAdmin("super_admin"), upload.fields([
+    {
+        name : "poster",
+        maxCount : 1
+    },
+    {
+        name : "trailer",
+        maxCount : 1
+    }
+]), addMovie);
+router.route("/:movieId").put(protectAdmin, isAdmin("super_admin"), upload.fields([
+    {
+        name : "poster",
+        maxCount : 1
+    },
+    {
+        name : "trailer",
+        maxCount : 1
+    }
+]),updateMovie);
+router.route("/:movieId").delete(protectAdmin, isAdmin("super_admin"), deleteMovie);
 export default router;
