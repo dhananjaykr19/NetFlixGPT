05ce8f84058376d3c6a85daf927bc65d6faf19c8
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-09-04 17:39:14 +0530

    role check

---
+++ backend/src/middlewares/isAdmin.middleware.js
@@ -0,0 +1,11 @@
+import { ApiError } from "../utils/ApiError.js";
+
+export const requireRole = (role) => {
+    return (req, res, next) => {
+        if(req.user && req.user.role === role){
+            next();
+        }else{
+            throw new ApiError(403, `Access denied. Only ${role}s allowed.`);
+        }
+    };
+};
